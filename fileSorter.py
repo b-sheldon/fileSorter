e4c1d68ec867632afc67ffe8b66c93c5822ff17a
Ben Sheldon <bsheldon02@gmail.com>
2023-12-20 12:28:44 -0600

    Update to sort Documents folder

--- fileSorter.py
+++ fileSorter.py
@@ -1,37 +1,77 @@
 import os
 import sys
 
-if len(sys.argv) != 2:
-    print("Incorrect Arguments Provided, use: python fileSorter.py targetDirectory")
-    exit()
-
-# Validate given argument is an existing directory
-target = sys.argv[1]
-if not os.path.isdir(target):
-    print("Invalid Directory Provided, use: python fileSorter.py targetDirectory")
+if len(sys.argv) != 3:
+    print("Incorrect Arguments Provided, use: python fileSorter.py sortType opt:targetDirectory")
     exit()
     
+if (sys.argv[1] != "default" and sys.argv[1] != 'Documents'):
+    print("Invalid Arguments Provided, use: python fileSorter.py sortType opt:targetDirectory")
+    exit()
 
-directories = { }
-# Create a list of all directories in the target directory
-for directory in os.listdir(target):
-    if not os.path.isfile(directory):
-        directories[directory] = directory
+if sys.argv[1] == "default":
+    # Validate given argument is an existing directory
+    target = sys.argv[2]
+    if not os.path.isdir(target):
+        print("Invalid Directory Provided, use: python fileSorter.py targetDirectory")
+        exit()
 
-# Loop through all files in the target directory
-for file in os.listdir(target):
-    if os.path.isfile(target + '/' + file):
-        # check to see if directory already exists of this file type
-        split = file.split( '.' )
-        newDirectory = split[ 1 ] + ' Files'
+    directories = { }
+    # Create a list of all directories in the target directory
+    for directory in os.listdir(target):
+        if not os.path.isfile(directory):
+            directories[directory] = directory
 
-        # Create a new directory for this file type if it doesn't exist
-        if newDirectory not in directories:
-            os.mkdir(target + '/' + newDirectory)
-        # Otherwise, add this directory to the known directories Dictionary
-        else:
-            directories[newDirectory] = newDirectory
+    # Loop through all files in the target directory
+    for file in os.listdir(target):
+        if os.path.isfile(target + '/' + file):
+            # check to see if directory already exists of this file type
+            split = file.split( '.' )
+            newDirectory = split[ 1 ] + ' Files'
 
-        # Move the file to the file type directory
-        os.rename(target + '/' + file, target + '/' + newDirectory + '/' + file)
+            # Create a new directory for this file type if it doesn't exist
+            if newDirectory not in directories:
+                os.mkdir(target + '/' + newDirectory)
+            # Otherwise, add this directory to the known directories Dictionary
+            else:
+                directories[newDirectory] = newDirectory
+
+            # Move the file to the file type directory
+            os.rename(target + '/' + file, target + '/' + newDirectory + '/' + file)
+
+# When using Documents sorting, the files should be named as follows: Category 1-Category 2-File Name.filetype
+elif sys.argv[1] == 'Documents':
+    target = os.path.expanduser('~') + '/Documents/' + sys.argv[2]
+
+    directories = { }
+    # Create a list of all directories in the target directory
+    for directory in os.listdir(target):
+        if not os.path.isfile(directory):
+            directories[directory] = directory
+
+    # Loop through all files in the target directory
+    for file in os.listdir(target):
+        if os.path.isfile(target + '/' + file):
+            # Check to see if directory already exists of this file type
+            split = file.split('-')
+
+            # Ignore files using the incorrect format
+            if len(split) < 3:
+                continue
+
+            property = split[0]
+
+            # Create a new directory for this property if it doesn't exist, add it to known directories
+            if property not in directories:
+                os.mkdir(target + '/' + property)
+                directories[property] = property
+            type = split[1]
+
+            # Check if this file type for this property exists, make a new directory if it doesn't
+            if property + '/' + type not in directories:
+                os.mkdir(target + '/' + property + '/' + type)
+                directories[property + '/' + type] = property + '/' + type
+
+            # Move the file to the correct directory
+            os.rename(target + '/' + file, target + '/' + property + '/' + type + '/' + split[2])
         
